7ce8d7f7c3f0cb3bff5b7c99af4c24777170ca0a
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-10-07 08:24:31 -0300

    class

--- sobracarga.py
+++ sobracarga.py
@@ -1,9 +1,2 @@
-# padrão 
-class Calculadora:
-    def soma(self, a=0, b=0, c=0):
-        return a + b + c
+class GeradorRelatorio:
 
-calc = Calculadora()
-print(calc.soma(2, 3))      # Soma dois números
-print(calc.soma(2, 3, 4))   # Soma três números
-print(calc.soma(5))         # Soma apenas um número
