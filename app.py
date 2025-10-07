32aea58b44e4f778addaedb5eea3a17be638ab91
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-10-07 10:29:38 -0300

    qq

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from sobracarga import*
 gerador = GeradorRelatorio()
 
 titulo= input("Insira um titulo, caso n queira de um entrer: \n-->")
-corpo=
-rodape=
-multi=input("Deseja inserir mais paragrafos? \nDigite: \n1-SIM \n2-NÃO \n--->")
+corpo=input("Insira um paragrafo: \n--->")
+multi=int(input("Deseja inserir mais paragrafos? \nDigite: \n1-SIM \n2-NÃO \n--->"))
 
 
-gerador.gerar(titulo)
+gerador.gerar(titulo= titulo, corpo=corpo, multi=multi)
+
--- sobracarga.py
+++ sobracarga.py
@@ -2,16 +2,21 @@ class GeradorRelatorio:
     def gerar(self,titulo=0, corpo=0, rodape=0, paragrafo=0, multi=0, metadados={}):
         while True:
             
-            if titulo == '':
-                print('')
-            else:
-                print(titulo)
+                if titulo == '':
+                    print('')
+                else:
+                    print(titulo)
+                    break
             
-        
-            if corpo == '':
-                print('')
-            else:
-                if multi ==1:
-                   paragrafo
-                print(corpo)
-
+                    
+                if not corpo :
+                    print('')
+                else:
+                    if multi ==1:
+                        for paragrafo in corpo:
+                            print(corpo)
+                    else:
+                        print(paragrafo)
+            
+                if rodape == '':
+                    print("rodape")
